63246f79b2c6a18b5c399128f4e8a02cf6d8b571
Richard Leurs <7275740+EarthlingRich@users.noreply.github.com>
2019-12-16 20:20:08 +0100

    Start React quiz project and do module 1.

---
+++ React/Project Quiz/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import Quiz from './Quiz'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Quiz />
+    )
+  }
+}
+
+export default App
