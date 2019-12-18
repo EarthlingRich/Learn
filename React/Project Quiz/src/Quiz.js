ff665eda98a8b97142b9f7a1d170febb51a58932
Richard Leurs <7275740+EarthlingRich@users.noreply.github.com>
2019-12-18 19:31:04 +0100

    Implement quiz project.

--- React/Project Quiz/src/Quiz.js
+++ React/Project Quiz/src/Quiz.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import QuizEnd from './QuizEnd'
+import QuizQuestion from './QuizQuestion'
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
@@ -8,10 +11,26 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
 
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 }
+        })
+    }
+
+    handleResetClick() {
+        this.setState({ quiz_position: 1 })
+    }
+
     render() {
+        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
+
         return (
             <div>
-                <div className='QuizQuestion'>{ quizData.quiz_questions[0].instruction_text }</div>
+                { isQuizEnd 
+                    ? <QuizEnd resetClickHandler={ this.handleResetClick.bind(this) }/>
+                    : <QuizQuestion 
+                        quiz_question={ quizData.quiz_questions[this.state.quiz_position - 1]}
+                        showNextQuestionHandler={ this.showNextQuestion.bind(this) }/> }
             </div>
         )
     }
---
+++ React/Project Quiz/src/QuizQuestion.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+import QuizQuestionButton from './QuizQuestionButton'
+
+class QuizQuestion extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { incorrectAnswer: false }
+    }
+
+    handleClick(buttonText) {
+        
+        if (this.props.quiz_question.answer === buttonText) {
+            this.props.showNextQuestionHandler()
+            this.setState({ incorrectAnswer: false })
+        }
+        else {
+            this.setState({ incorrectAnswer: true })
+        }
+    }
+
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>{ this.props.quiz_question.instruction_text }</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        { this.props.quiz_question.answer_options.map((answerOption, index) => {
+                            return <QuizQuestionButton button_text={ answerOption } clickHandler={ this.handleClick.bind(this) } key={ index } />
+                        }) }
+                    </ul>
+                </section>
+                { this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p>: null }
+            </main>
+        )
+    }
+}
+
+export default QuizQuestion
