fa421633deb6152271ffb5eaa644cc51a0fa62bd
Richard Leurs <7275740+EarthlingRich@users.noreply.github.com>
2019-12-21 19:45:54 +0100

    Make click counter example.

---
+++ React/React Fundamentals/2. Introducing React/click-counter/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+let model = { clicks: 0 };
+
+function render() {
+    ReactDOM.render(<App clicks={model.clicks} onClick={() => {model.clicks += 1; render();}}/>, document.getElementById('root'));
+}
+render();
